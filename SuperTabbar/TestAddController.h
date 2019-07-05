88c291294cdc915a474bc30cfb8e0835fc580694
xuchao <1224832958@qq.com>
2019-07-05 10:08:19 +0800

    add TestVC

---
+++ SuperTabbar/TestAddController.h
@@ -0,0 +1,17 @@
+//
+//  TestAddController.h
+//  SuperTabbar
+//
+//  Created by 李朋朋 on 2019/7/5.
+//  Copyright © 2019 sd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestAddController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SuperTabbar/TestAddController.m
@@ -0,0 +1,13 @@
+//
+//  TestAddController.m
+//  SuperTabbar
+//
+//  Created by 李朋朋 on 2019/7/5.
+//  Copyright © 2019 sd. All rights reserved.
+//
+
+#import "TestAddController.h"
+
+@implementation TestAddController
+
+@end
